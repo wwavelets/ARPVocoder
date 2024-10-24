05cbef2f69c07a549b727d7dae5190609da447ce
whattonamemyself <erichsu467@gmail.com>
2024-10-24 00:17:15 -0700

    jucelibrarycode

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,312 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   _0_png;
+    const int            _0_pngSize = 100;
+
+    extern const char*   _1_png;
+    const int            _1_pngSize = 434;
+
+    extern const char*   _2_png;
+    const int            _2_pngSize = 316;
+
+    extern const char*   _3_png;
+    const int            _3_pngSize = 1091;
+
+    extern const char*   _4_png;
+    const int            _4_pngSize = 1451;
+
+    extern const char*   _5_png;
+    const int            _5_pngSize = 1522;
+
+    extern const char*   _6_png;
+    const int            _6_pngSize = 1358;
+
+    extern const char*   _7_png;
+    const int            _7_pngSize = 216;
+
+    extern const char*   _8_png;
+    const int            _8_pngSize = 754;
+
+    extern const char*   _9_png;
+    const int            _9_pngSize = 750;
+
+    extern const char*   _10_png;
+    const int            _10_pngSize = 754;
+
+    extern const char*   _11_png;
+    const int            _11_pngSize = 471;
+
+    extern const char*   _12_png;
+    const int            _12_pngSize = 313;
+
+    extern const char*   _13_png;
+    const int            _13_pngSize = 221;
+
+    extern const char*   _14_png;
+    const int            _14_pngSize = 238;
+
+    extern const char*   _15_png;
+    const int            _15_pngSize = 517;
+
+    extern const char*   _16_png;
+    const int            _16_pngSize = 1210;
+
+    extern const char*   _17_png;
+    const int            _17_pngSize = 366;
+
+    extern const char*   _18_png;
+    const int            _18_pngSize = 1033;
+
+    extern const char*   _19_png;
+    const int            _19_pngSize = 1248;
+
+    extern const char*   _20_png;
+    const int            _20_pngSize = 758;
+
+    extern const char*   _21_png;
+    const int            _21_pngSize = 1083;
+
+    extern const char*   _22_png;
+    const int            _22_pngSize = 1361;
+
+    extern const char*   _23_png;
+    const int            _23_pngSize = 609;
+
+    extern const char*   _24_png;
+    const int            _24_pngSize = 1353;
+
+    extern const char*   _25_png;
+    const int            _25_pngSize = 1347;
+
+    extern const char*   _26_png;
+    const int            _26_pngSize = 421;
+
+    extern const char*   _27_png;
+    const int            _27_pngSize = 509;
+
+    extern const char*   _28_png;
+    const int            _28_pngSize = 676;
+
+    extern const char*   _29_png;
+    const int            _29_pngSize = 350;
+
+    extern const char*   _30_png;
+    const int            _30_pngSize = 676;
+
+    extern const char*   _31_png;
+    const int            _31_pngSize = 964;
+
+    extern const char*   _32_png;
+    const int            _32_pngSize = 2232;
+
+    extern const char*   _33_png;
+    const int            _33_pngSize = 886;
+
+    extern const char*   _34_png;
+    const int            _34_pngSize = 939;
+
+    extern const char*   _35_png;
+    const int            _35_pngSize = 1282;
+
+    extern const char*   _36_png;
+    const int            _36_pngSize = 907;
+
+    extern const char*   _37_png;
+    const int            _37_pngSize = 476;
+
+    extern const char*   _38_png;
+    const int            _38_pngSize = 377;
+
+    extern const char*   _39_png;
+    const int            _39_pngSize = 1402;
+
+    extern const char*   _40_png;
+    const int            _40_pngSize = 413;
+
+    extern const char*   _41_png;
+    const int            _41_pngSize = 174;
+
+    extern const char*   _42_png;
+    const int            _42_pngSize = 526;
+
+    extern const char*   _43_png;
+    const int            _43_pngSize = 888;
+
+    extern const char*   _44_png;
+    const int            _44_pngSize = 267;
+
+    extern const char*   _45_png;
+    const int            _45_pngSize = 977;
+
+    extern const char*   _46_png;
+    const int            _46_pngSize = 726;
+
+    extern const char*   _47_png;
+    const int            _47_pngSize = 1394;
+
+    extern const char*   _48_png;
+    const int            _48_pngSize = 706;
+
+    extern const char*   _49_png;
+    const int            _49_pngSize = 1542;
+
+    extern const char*   _50_png;
+    const int            _50_pngSize = 873;
+
+    extern const char*   _51_png;
+    const int            _51_pngSize = 1329;
+
+    extern const char*   _52_png;
+    const int            _52_pngSize = 281;
+
+    extern const char*   _53_png;
+    const int            _53_pngSize = 710;
+
+    extern const char*   _54_png;
+    const int            _54_pngSize = 889;
+
+    extern const char*   _55_png;
+    const int            _55_pngSize = 1269;
+
+    extern const char*   _56_png;
+    const int            _56_pngSize = 1097;
+
+    extern const char*   _57_png;
+    const int            _57_pngSize = 748;
+
+    extern const char*   _58_png;
+    const int            _58_pngSize = 692;
+
+    extern const char*   _59_png;
+    const int            _59_pngSize = 304;
+
+    extern const char*   _60_png;
+    const int            _60_pngSize = 528;
+
+    extern const char*   _61_png;
+    const int            _61_pngSize = 306;
+
+    extern const char*   _62_png;
+    const int            _62_pngSize = 513;
+
+    extern const char*   _63_png;
+    const int            _63_pngSize = 101;
+
+    extern const char*   _64_png;
+    const int            _64_pngSize = 320;
+
+    extern const char*   _65_png;
+    const int            _65_pngSize = 978;
+
+    extern const char*   _66_png;
+    const int            _66_pngSize = 982;
+
+    extern const char*   _67_png;
+    const int            _67_pngSize = 924;
+
+    extern const char*   _68_png;
+    const int            _68_pngSize = 977;
+
+    extern const char*   _69_png;
+    const int            _69_pngSize = 967;
+
+    extern const char*   _70_png;
+    const int            _70_pngSize = 486;
+
+    extern const char*   _71_png;
+    const int            _71_pngSize = 1281;
+
+    extern const char*   _72_png;
+    const int            _72_pngSize = 544;
+
+    extern const char*   _73_png;
+    const int            _73_pngSize = 318;
+
+    extern const char*   _74_png;
+    const int            _74_pngSize = 432;
+
+    extern const char*   _75_png;
+    const int            _75_pngSize = 727;
+
+    extern const char*   _76_png;
+    const int            _76_pngSize = 168;
+
+    extern const char*   _77_png;
+    const int            _77_pngSize = 701;
+
+    extern const char*   _78_png;
+    const int            _78_pngSize = 499;
+
+    extern const char*   _79_png;
+    const int            _79_pngSize = 994;
+
+    extern const char*   _80_png;
+    const int            _80_pngSize = 1019;
+
+    extern const char*   _81_png;
+    const int            _81_pngSize = 1010;
+
+    extern const char*   _82_png;
+    const int            _82_pngSize = 342;
+
+    extern const char*   _83_png;
+    const int            _83_pngSize = 981;
+
+    extern const char*   _84_png;
+    const int            _84_pngSize = 497;
+
+    extern const char*   _85_png;
+    const int            _85_pngSize = 537;
+
+    extern const char*   _86_png;
+    const int            _86_pngSize = 670;
+
+    extern const char*   _87_png;
+    const int            _87_pngSize = 1001;
+
+    extern const char*   _88_png;
+    const int            _88_pngSize = 852;
+
+    extern const char*   _89_png;
+    const int            _89_pngSize = 896;
+
+    extern const char*   _90_png;
+    const int            _90_pngSize = 605;
+
+    extern const char*   _91_png;
+    const int            _91_pngSize = 696;
+
+    extern const char*   _92_png;
+    const int            _92_pngSize = 132;
+
+    extern const char*   _93_png;
+    const int            _93_pngSize = 710;
+
+    extern const char*   _94_png;
+    const int            _94_pngSize = 614;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 95;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
